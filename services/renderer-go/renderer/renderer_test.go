64802ff0f7aad6851fb6543cdde65819f5df953e
taiyoslime <t@iyosli.me>
2020-08-24 18:35:32 +0900

    renderer-goにmarkdown記法を追加
    renderer-goにmarkdown記法を追加

--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -8,8 +8,23 @@ import (
 )
 
 func Test_Render(t *testing.T) {
-	src := `foo https://google.com/ bar`
+	src := `
+# h1
+## h2
+### h3
+- hoge
+- fuga
+- [piyo](https://taiyosli.me)
+`
 	html, err := Render(context.Background(), src)
 	assert.NoError(t, err)
-	assert.Equal(t, `foo <a href="https://google.com/">https://google.com/</a> bar`, html)
+	assert.Equal(t, `<h1 id="h1">h1</h1>
+<h2 id="h2">h2</h2>
+<h3 id="h3">h3</h3>
+<ul>
+<li>hoge</li>
+<li>fuga</li>
+<li><a href="https://taiyosli.me">piyo</a></li>
+</ul>
+`, html)
 }
