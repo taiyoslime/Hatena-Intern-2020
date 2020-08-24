a8fd89bcb1490058e90c6db274db437972aa7a98
taiyoslime <t@iyosli.me>
2020-08-24 18:35:32 +0900

    renderer-goにmarkdown記法を追加
    renderer-goにmarkdown記法を追加

--- services/renderer-go/grpc/server_test.go
+++ services/renderer-go/grpc/server_test.go
@@ -10,8 +10,23 @@ import (
 
 func Test_Server_Render(t *testing.T) {
 	s := NewServer()
-	src := `foo https://google.com/ bar`
+	src := `
+# h1
+## h2
+### h3
+- hoge
+- fuga
+- [piyo](https://taiyosli.me)
+`
 	reply, err := s.Render(context.Background(), &pb.RenderRequest{Src: src})
 	assert.NoError(t, err)
-	assert.Equal(t, `foo <a href="https://google.com/">https://google.com/</a> bar`, reply.Html)
+	assert.Equal(t, `<h1 id="h1">h1</h1>
+<h2 id="h2">h2</h2>
+<h3 id="h3">h3</h3>
+<ul>
+<li>hoge</li>
+<li>fuga</li>
+<li><a href="https://taiyosli.me">piyo</a></li>
+</ul>
+`, reply.Html)
 }
--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -3,23 +3,30 @@ package renderer
 import (
 	"bytes"
 	"context"
-	"html/template"
-	"regexp"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
 )
-
-var urlRE = regexp.MustCompile(`https?://[^\s]+`)
-var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
 
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
-	// TODO: これはサンプル実装 (URL の自動リンク) です
-	html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
-		var w bytes.Buffer
-		err := linkTmpl.ExecuteTemplate(&w, "link", url)
-		if err != nil {
-			return url
-		}
-		return w.String()
-	})
+
+	parser := goldmark.New(
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+		),
+	)
+
+	var buf bytes.Buffer
+	if err := parser.Convert([]byte(src), &buf); err != nil {
+		return src, err
+	}
+
+	html := buf.String()
 	return html, nil
 }
