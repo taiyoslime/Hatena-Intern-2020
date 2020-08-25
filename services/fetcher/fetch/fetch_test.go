84f117cb32140e96838bcafb0f6e261a940463d8
taiyoslime <t@iyosli.me>
2020-08-25 18:36:51 +0900

    fetcher: 異常系に関するテストを追加

--- services/fetcher/fetch/fetch_test.go
+++ services/fetcher/fetch/fetch_test.go
@@ -7,9 +7,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type FetchTestCase struct {
+	in  string
+	out string
+	err bool
+}
+
+var fetchTestCases = []FetchTestCase{
+	{
+		in:  "https://google.com",
+		out: "Google",
+		err: false,
+	},
+	{
+		// <title>が無い場合
+		in:  "http://dev.taiyosli.me",
+		out: "",
+		err: false,
+	},
+	{
+		// 存在しないurlの場合
+		in:  "https://url.which.does.not.exist",
+		out: "",
+		err: true,
+	},
+}
+
 func Test_Fetch(t *testing.T) {
-	url := "https://google.com"
-	html, err := Fetch(context.Background(), url)
-	assert.NoError(t, err)
-	assert.Equal(t, `Google`, html)
+	for _, testCase := range fetchTestCases {
+		url := testCase.in
+		title, err := Fetch(context.Background(), url)
+		if !testCase.err {
+			assert.NoError(t, err)
+		} else {
+			assert.Error(t, err)
+		}
+
+		assert.Equal(t, title, testCase.out)
+	}
 }
